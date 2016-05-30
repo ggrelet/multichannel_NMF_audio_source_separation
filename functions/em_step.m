2fd6f9dfda38293e1a9e6914279b7b99a0155bbc
ggrelet <guillaume.grelet@gmail.com>
2016-05-30 17:51:45 +0200

    Revert "Correction em_step"
    This reverts commit 072cc4adce82d7e9d45b872650e22ecdf7a024fb.

--- functions/em_step.m
+++ functions/em_step.m
@@ -1,17 +1,16 @@
-function [A_new,W_new,H_new,s_new,sigb_new] =em_step(x,A,W,H,s,sigb)
+function [A_new,W_new,H_new,s_new] =em_step(x,A,W,H,s,sigb)
 
-    [I,F,N]=size(x); % Nombre de chaines*nombre de fréquence*nombre de tps
-    [~,J,~]=size(A); % Nombre d'instruments
-    K=15; %ordre de la NMF
+    [I,F,N]=size(x);
     Rxx=zeros(I,I,F);
     Rxs=zeros(I,J,F);
     Rss=zeros(J,J,F);
+    sigs=zeros(J,J,F,N);
+    Gs=zeros(J,J,F,N);
     for n=1:N
         for f=1:F
-            sigs=zeros(J);
-            sigx=A(:,:,f)*sigs*A(:,:,f)'+sigb(f);
-            sigc=diag(W(f,:)*H(:,n));
-            Gs=sigs*A(:,:,f)/(sigx);
+            sigs=
+            sigx=A(:,:,f)*sigs*A(:,:,f)'+sigb(f)
+            Gs=sigs*A(:,:,f)*inv(sigx)
             Rxx(:,:,f)=Rxx(:,:,f)+x(:,f,n)*x(:,f,n)';
             Rxs(:,:,f)=Rxs(:,:,f)+x(:,f,n)*s(:,f,n)';
             Rss(:,:,f)=Rss(:,:,f)+s(:,f,n)*s(:,f,n)'+sigs-Gs*A(:,:,f)*sigs;
