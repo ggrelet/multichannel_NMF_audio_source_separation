327de4c6ada50ef06f16d3ae6c1859185d81a5bb
fdarmon <francois.darmon@telecom-paristech.fr>
2016-05-31 18:03:58 +0200

    Plus que la normalisation sur em_step

--- functions/em_step.m
+++ functions/em_step.m
@@ -1,12 +1,20 @@
 function [A_new,W_new,H_new,s,sigb_new] =em_step(x,A,W,H,sigb)
 
+
+%%
+% Une interation de EM
 [I,F,N]=size(x); % Nombre de chaines*nombre de frÃ©quence*nombre de tps
 [~,J,~]=size(A); % Nombre d'instruments
 K_partition=[5 5 5 5];
 K_cumsum = cumsum(K_partition);
 K=sum(K_partition);
-s= %% A INITIALISER
-c= %% A INITIALISER
+s= zeros(J,F,N);
+c= zeros(K,F,N); %% Pas sur
+A_new = zeros(size(A));
+sigb_new = zeros(size(sigb));
+u=zeros(K,F,N);
+H_new=zeros(size(H));
+W_new=zeros(size(W));
 
 for f=1:F
     Rxx=zeros(I,I);
@@ -33,20 +41,42 @@ for f=1:F
         sigc=diag(W(f,:)*H(:,n));
         Gs=sigs*A(:,:,f)'/(sigx);
         Gc=sigc*A_ronde'/sigx;
-        s=Gs*x(:,f,n);
-        c=Gc*x(:,f,n);
+        s(:,f,n)=Gs*x(:,f,n);
+        c(:,f,n)=Gc*x(:,f,n);
         Rxx(:,:)=Rxx(:,:)+x(:,f,n)*x(:,f,n)'/N;
         Rxs(:,:)=Rxs(:,:)+x(:,f,n)*s(:,f,n)'/N;
         Rss(:,:)=Rss(:,:)+(s(:,f,n)*s(:,f,n)'+sigs-Gs*A(:,:,f)*sigs)/N;
-        u(:,n)=diag(c(:,f,n)*c(:,f,n)'+sigc-Gc*A_ronde*sigc);
-        
+        u(:,f,n)=diag(c(:,f,n)*c(:,f,n)'+sigc-Gc*A_ronde*sigc);
         
     end
     %% Mstep
+    temp=Rxs/Rss;
+    A_new(:,:,f)=temp;
+    sigb_new(:,:,f)=diag(Rxx-temp*Rxs'-Rxs*temp+temp*Rss*temp');%% WTF scalaire ?
+    for k=1:K
+        W_new(f,k)=sum(u(k,f,:)./h(k,:))/N;
+    end
     
 end
+for n=1:N
+    for k=1:K      
+        H_new(k,n)=sum(u(k,:,n)./W_new(:,k))/F;
+    end
+end
 
-%% M step
+%% Normalization of A_new W_new and H_new
+D=zeros(J,J,F);
+for f=1:F
+   
+   for j=1:J
+      D(j,j,f)=sqrt(sum(abs(A_new(:,j,f).^2)))*exp(1i*arg(A_new(1,j,f))); 
+   end
+   A_new(:,:,f)=A_new(:,:,f)/D(:,:,f); 
+   % on a ainsi la sommes sur i  A_ij,f = 1 et A_1j réel > 0 
+end
+% Finir la normalisation
+
+
 
 
 
