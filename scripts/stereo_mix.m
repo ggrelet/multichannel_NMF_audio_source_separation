ff92708047bffe39dedb99dee52fb9fb79d1eeea
ggrelet <guillaume.grelet@gmail.com>
2016-06-02 18:28:07 +0200

    Modifications mineures

--- scripts/stereo_mix.m
+++ scripts/stereo_mix.m
@@ -1,11 +1,13 @@
 close all
 clear all
 
+% Open sources
 [drum, Fd]  = audioread('sources/drum.wav');
 [piano, Fp] = audioread('sources/piano.wav');
 [voice, Fv] = audioread('sources/voice.wav');
 load('sources/mixing_filters_ozerov.mat');
 
+% Get channels
 A11 = squeeze(A(1,1,:));
 A12 = squeeze(A(1,2,:));
 A13 = squeeze(A(1,3,:));
@@ -14,9 +16,11 @@ A21 = squeeze(A(2,1,:));
 A22 = squeeze(A(2,2,:));
 A23 = squeeze(A(2,3,:));
 
+% Make mixed instruments with convolutive coefficients from
+% mixin_filter_ozerov.mat
 piano_mixed = mix(piano, 1);
 drum_mixed = mix(drum, 2);
 voice_mixed = mix(voice, 3);
 
-fmix = piano_mixed + drum_mixed + voice_mixed;
-omix = piano + drum + voice;
+fmix = piano_mixed + drum_mixed + voice_mixed; % Filtered mix
+omix = piano + drum + voice; % Original mix
