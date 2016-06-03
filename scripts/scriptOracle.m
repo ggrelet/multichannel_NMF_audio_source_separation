31533457f783f18ff2b923cc4c5c2a083b4f2579
Francois Darmon <francois.darmon@telecom-paristech.fr>
2016-06-03 11:54:36 +0200

    WIP script principal

--- scripts/scriptOracle.m
+++ scripts/scriptOracle.m
@@ -1,2 +1,26 @@
 clear all
 close all
+
+
+%% Open sources
+[drum, Fd]  = audioread('sources/drum.wav');
+[piano, Fp] = audioread('sources/piano.wav');
+[voice, Fv] = audioread('sources/voice.wav');
+load('sources/mixing_filters_ozerov.mat');
+
+
+%% Convolutive mixing
+% from mixin_filter_ozerov.mat
+piano_mixed = mix(piano, 1);
+drum_mixed = mix(drum, 2);
+voice_mixed = mix(voice, 3);
+
+fmix = piano_mixed + drum_mixed + voice_mixed; % Filtered mix
+omix = piano + drum + voice; % Instantaneous mix
+
+%% Spectrogram
+X=spec(fmix,1024,0.5);
+
+%% Initialisation de l'algo
+
+%% Algo sur critère d'arret 
