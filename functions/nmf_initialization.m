5568e156e63ded3fc54a98e669bfac5c27520361
ggrelet <guillaume.grelet@gmail.com>
2016-06-15 17:58:48 +0200

    Modification de nmf_initialisation et de son critère d'arrêt

--- functions/nmf_initialization.m
+++ functions/nmf_initialization.m
@@ -1,4 +1,4 @@
-function [W, H] = nmf_initialization(V, betaparam, stop,order)
+function [W, H] = nmf_initialization(V, betaparam, stop, order)
 %NMF_INITIALIZATION Summary of this function goes here
 %   V : input matrix 
 %   betaparam : as it is called in the algorithm
@@ -11,19 +11,16 @@ s2 = size(V, 2);
 W = rand(s1, order);
 H = rand(order, s2);
 
-Wdiff = 1;
-Hdiff = 1;
+Vdiff = 1;
 compteur=1;
-while (Hdiff > stop && Wdiff > stop) || compteur>1000
+while (Vdiff > stop) || compteur>1000
     compteur=compteur+1;
-    H1 = H;
-    W1 = W;
-
+    
     H = H.*(W'*((W*H).^(betaparam-2).*V))./(W'*(W*H).^(betaparam-1));
     W = W.*(((W*H).^(betaparam-2).*V)*H')./((W*H).^(betaparam-1)*H');
     
-    Hdiff = norm(H1-H);
-    Wdiff = norm(W1-W);
+    V2 = W*H;
+    Vdiff = norm(V-V2);
 end
 end
 
