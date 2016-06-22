a0bd9e75ed8e89fd7da3d97ce886ef607d584d26
Francois Darmon <francois.darmon@telecom-paristech.fr>
2016-06-22 19:34:14 +0200

    Correction de ispec

--- functions/ispec.m
+++ functions/ispec.m
@@ -2,18 +2,19 @@ function [vector] = ispec(X, window_length)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-vector = [];
+vector = zeros(size(X,2)*window_length/2,1);
 
 w = zeros(window_length, 1);
 for k=1:window_length
     w(k) = sin((pi * k)/(window_length - 1)); % Sine-window calculation
 end
 
-for k = 1:2:size(X, 2)
-  
+for k = 1:size(X, 2)-1
+    
     A = ifft(X(:, k));
-    A = A./w;
-    vector = [vector ; A];
+    A = A.*w;
+    vector((k-1)*window_length/2+1:(k-1)*window_length/2+window_length)=...
+        vector((k-1)*window_length/2+1:(k-1)*window_length/2+window_length)+A;
     
 end
 
